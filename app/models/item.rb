e6f62e450efad3bc4e1f059aa0fb7abc6c667945
itotamami <ito@exmedia.jp>
2015-11-02 01:10:31 +0000

    Lesson8 monolist 6.7 Implementation Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,16 @@ class Item < ActiveRecord::Base
 
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+
+  #below lines add L8.6.7
+  has_many :wants,class_name:"Want",foreign_key:"item_id" , dependent: :destroy
+  #外部キーが使われているときは、外部キーの削除に連動させるため基本的にdependent:   :destroyもつける
+  #typeがWantである行の集まりを仮のテーブルwants want_usersとの仮想的な中間テーブルとなる
+  has_many :want_users,through: :wants,source: :user
+  has_many :haves,class_name:"Have",foreign_key:"item_id" , dependent: :destroy
+  #外部キーが使われているときは、外部キーの削除に連動させるため基本的にdependent:   :destroyもつける
+  #typeがHaveであるものの一覧がhaves ,have_usersとの仮想的な中間テーブルとなる
+  has_many :have_users,through: :haves,source: :user
+
+  #above lines add L8.6.7
 end
